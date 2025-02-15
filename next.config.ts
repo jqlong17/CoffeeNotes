9d086de5fd311cc07000b661921310bf605540d1
ruska <longjiaqiuu@gmail.com>
2025-02-15 22:46:21 +0800

    fix: 重构目录结构和配置以解决构建问题

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 export default nextConfig;
